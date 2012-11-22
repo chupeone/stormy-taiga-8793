9003e59e02e9b6c20a34ece74253ca2034b52899
Juan Francisco Echeverría <chupeone@gmail.com>
2012-11-22 19:58:31 +0000

    like button!

--- index.php
+++ index.php
@@ -14,6 +14,8 @@
   js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=394124587331220";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
+<div id="fb-root"></div>
+<div class="fb-like" data-href="https://stormy-taiga-8793.herokuapp.com/" data-send="true" data-width="450" data-show-faces="true"></div>
 <div class="fb-facepile" data-href="http://stormy-taiga-8793.herokuapp.com" data-action="any" data-max-rows="1" data-width="300"></div>
 
     
