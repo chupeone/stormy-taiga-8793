2a82f6623f5868ee10230a05f98621e1c72c95db
Juan Francisco Echeverría <chupeone@gmail.com>
2013-01-10 21:10:22 +0000

    new goal 2

--- goal1.php
+++ goal1.php
@@ -1,6 +1,6 @@
  <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# exampleopengraph: http://ogp.me/ns/fb/exampleopengraph#">
   <meta property="fb:app_id" content="394124587331220" /> 
   <meta property="og:type"   content="exampleopengraph:goal" /> 
-  <meta property="og:url"    content="https://stormy-taiga-8793.herokuapp.com/" /> 
-  <meta property="og:title"  content="Quit Drinking" /> 
+  <meta property="og:url"    content="http://stormy-taiga-8793.herokuapp.com/goal1.php" /> 
+  <meta property="og:title"  content="Quit Drinking	" /> 
   <meta property="og:image"  content="http://1.bp.blogspot.com/-oIKknQ--5_A/UEirie9FB8I/AAAAAAAAAE4/oIeRbPbvHaM/s332/no%2Balcohol%2Bsign.jpg" /> 
