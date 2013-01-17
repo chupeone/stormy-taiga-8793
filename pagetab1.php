e13a72e0be1006ca78ffba0147fb9673e7093821
Juan Francisco Echeverría <chupeone@gmail.com>
2013-01-17 20:17:24 +0000

    face

--- pagetab1.php
+++ pagetab1.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html>
-<body>
+<?php 
 
-<h1>My First Heading</h1>
 
-<p>My first paragraph.</p>
 
-</body>
-</html>
+print_r(parse_signed_request($_REQUEST['signed_request']));
+function parse_signed_request($signed_request) {
+  list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
 
- 
+  // decode the data
+  $sig = base64_url_decode($encoded_sig);
+  $data = json_decode(base64_url_decode($payload), true);
+
+  return $data;
+}
+
+function base64_url_decode($input) {
+  return base64_decode(strtr($input, '-_', '+/'));
+}
