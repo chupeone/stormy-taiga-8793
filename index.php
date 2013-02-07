eef596f8a85232fdf9ae24e25524d5f304bf02e9
Juan Francisco Echeverría <chupeone@gmail.com>
2013-02-07 17:31:20 +0000

    Added the status retrieval

--- index.php
+++ index.php
@@ -20,7 +20,13 @@ PROBANDO UNO DOS TRESS
 <div id="fb-root"></div>
 <div class="fb-like" data-href="https://stormy-taiga-8793.herokuapp.com/" data-send="true" data-width="450" data-show-faces="true"></div>
 <div class="fb-facepile" data-href="http://stormy-taiga-8793.herokuapp.com" data-action="any" data-max-rows="1" data-width="300"></div>
-
+<?php 
+ $result = $facebook->api_client->fql_query("SELECT status FROM user WHERE uid = YOURIDNUMBER");
+ // OR --- they both get the same data
+ $result = $facebook->api_client->users_getInfo(YOURIDNUMBER,'status');
+ print_r($result);
+ echo "<pre>Debug:" . print_r($facebook,true) . "</pre>"; // debug info
+?>
     
 </body>
 </html>
